e0440a9c6458d3baa5e65f88e158b0562c7df3ae
Adam Yarger <yargeradam@gmail.com>
2021-05-23 23:22:21 -0700

    adds emoji as favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,6 +37,10 @@ function App({ Component, pageProps }: AppProps) {
       <MDXProvider>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&amp;family=Poppins:ital,wght@0,500;1,500&amp;display=swap" rel="stylesheet"></link>
+          <link
+            rel="icon"
+            href={`data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>`}
+          />
         </Head>
         <Component {...pageProps} />
       </MDXProvider>
